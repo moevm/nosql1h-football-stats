b9843d5844f772b400b55abcf5f5523963dcd7b8
Jenesius <lokargenia@gmail.com>
2019-04-22 01:52:19 +0300

    firs step

---
+++ app/main/db.py
@@ -0,0 +1,63 @@
+from pymongo import MongoClient
+import pprint
+
+client = MongoClient()
+db = client['NoSQL']
+EPL = db["match"]
+
+def getTeamDetails(teamName):
+    result = EPL.aggregate([
+        {"$match": {"$or": [{"AwayTeam": teamName}, {"HomeTeam": teamName}]}},
+        {"$group": {
+            "_id": "null",
+            "Matches": {"$sum": 1},
+            "AC": {"$sum": "$AC"},
+            "AF": {"$sum": "$AF"},
+            "AR": {"$sum": "$AR"},
+            "AS": {"$sum": "$AS"},
+            "AST": {"$sum": "$AST"},
+            "AY": {"$sum": "$AY"},
+            "FTAG": {"$sum": "$FTAG"},
+            "FTHG": {"$sum": "$FTHG"},
+            "FTR": {"$sum": "$FTR"},
+            "HC": {"$sum": "$HC"},
+            "HF": {"$sum": "$HF"},
+            "HR": {"$sum": "$HR"},
+            "HS": {"$sum": "$HS"},
+            "HST": {"$sum": "$HST"},
+            "HTAG": {"$sum": "$HTAG"},
+            "HTHG": {"$sum": "$HTHG"},
+            "HTR": {"$sum": "$HTR"},
+            "HY": {"$sum": "$HY"},
+        }
+        }
+    ])
+
+    return result
+
+
+def compareTwoTeams(teamA, teamB):
+    result = EPL.find(
+        {"$or": [
+            {"$and": [{"AwayTeam": teamA}, {"HomeTeam": teamB}]},
+            {"$and": [{"AwayTeam": teamB}, {"HomeTeam": teamA}]}
+        ]},
+        {"_id": 0}) \
+        .sort("Date")
+    return result
+
+def findAllTeams():
+	result = EPL.distinct("AwayTeam");
+	return result
+def findMatchs(properties):
+	match = EPL.find(
+		{"Date":"2010-08-14"}
+	)
+
+	return match
+def findTeam(properties):
+    result = EPL.find(
+        properties,
+        {"_id": 0, "AwayTeam": 1, "HomeTeam": 1}) \
+        .sort("Date")
+    return result
---
+++ app/main/routes.py
@@ -0,0 +1,24 @@
+from app import app
+from flask import render_template, session, request, redirect, url_for, abort, current_app, flash, jsonify
+from app.main import db
+from bson.json_util import dumps
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+@app.route('/teams')
+def show_teams():
+	teams = db.findAllTeams()
+	return render_template('teams.html',teams = teams);
+@app.route('/findmatch')
+def find_match():
+	return render_template('findmatch.html')
+	
+@app.route('/findmatch/find/', methods=['POST'])
+def show_match():
+	post_data = request.json
+	result = dumps(db.findMatchs(post_data))
+	print(result)
+	return result
